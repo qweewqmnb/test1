e43a53ffa12144f648046702ecbb2fc7d03d01fe
oo <uioertert@163.com>
2018-11-22 20:56:53 +0800

    2056修改修改修改,新增方法

--- src/main/java/com/test/bean/abc.java
+++ src/main/java/com/test/bean/abc.java
@@ -5,4 +5,9 @@ public class abc {
 	public static void main(String[] args) {
 		System.out.println("hello");
 	}
+	
+//	修改修改修改修改修改修改修改修改修改
+	public void test() {
+		System.out.println("11111111122222222");
+	}
 }
